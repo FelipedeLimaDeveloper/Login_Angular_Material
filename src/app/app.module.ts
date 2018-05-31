31de510930590cd7f4bbdf43d0a7882cf8cc3803
FelipedeLimaDeveloper <felipedelima.developer@gmail.com>
2018-05-31 18:39:57 -0300

    Add: components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
